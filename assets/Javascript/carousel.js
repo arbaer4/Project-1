710155e52a2f23b2a2985d7a929147884a4a811c
Cesar "Tito" Lopez <cesar.lopez.rodriguez@gmail.com>
2019-06-17 20:08:01 -0400

    last version of slider ....

--- assets/Javascript/carousel.js
+++ assets/Javascript/carousel.js
@@ -2,3 +2,19 @@ $(document).ready(function(){
     // init carousel
   $('.carousel').carousel()
   });
+
+  // $(document).ready(function(){
+  //   // init carousel
+  // $('.carousel.carousel-slider').carousel({fullwidth : true})
+  // });
+
+
+
+
+  function btnClick()
+  {
+      //    button click
+      $("#btnSubmit").button().click(function(showForm){
+          alert("button");
+      });    
+  }
